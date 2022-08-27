1da80e324d39b7dc134299175b861a9f45286524
The-tired-gamer <98146541+The-tired-gamer@users.noreply.github.com>
2022-08-27 10:47:49 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,20 @@
+#Check the java version
+echo "--------------------------------------------------------------------------------------------------"
 java --version
+echo "--------------------------------------------------------------------------------------------------"
 
+#Warn you that the server will start.
+echo "Starting in five seconds, press Ctrl-C to cancel."
+sleep 1
+echo "Starting in four seconds, press Ctrl-C to cancel."
+sleep 1
+echo "Starting in three seconds, press Ctrl-C to cancel."
+sleep 1
+echo "Starting in two seconds, press Ctrl-C to cancel."
+sleep 1
+echo "Starting in one second, press Ctrl-C to cancel."
+sleep 
+echo "Starting, press Ctrl-C to cancel."
+
+#Starting the server
 java -Xms1G -Xmx2G -jar server.jar
