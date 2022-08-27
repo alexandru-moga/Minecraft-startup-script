1ad5d7f7b9d076325e18bdae6c8143a780070ffe
The-tired-gamer <98146541+The-tired-gamer@users.noreply.github.com>
2022-08-27 15:17:05 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -15,16 +15,7 @@ C:
 cd C:\Users\Windows\Desktop\server\
 
 #Warn you that the server will start.
-echo Starting in five seconds, press Ctrl-C to cancel.
-timeout 1
-echo Starting in four seconds, press Ctrl-C to cancel.
-timeout 1
-echo Starting in three seconds, press Ctrl-C to cancel.
-timeout 1
-echo Starting in two seconds, press Ctrl-C to cancel.
-timeout 1
-echo Starting in one second, press Ctrl-C to cancel.
-timeout 1 
+timeout 5 
 echo Starting, press Ctrl-C to cancel.
 
 
