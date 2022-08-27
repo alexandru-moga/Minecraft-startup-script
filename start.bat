e5949f83cee2848ede1153b836e4dd7fa2983362
The-tired-gamer <98146541+The-tired-gamer@users.noreply.github.com>
2022-08-27 13:23:27 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,35 @@
-java --version
+@echo off
 
-java -Xms1G -Xmx2G -jar forge-1.8-XYZ-universal.jar
+#Set window title
+title Server Console
+
+
+#Check the java version
+echo --------------------------------------------------------------------------------------------------
+java --version
+echo --------------------------------------------------------------------------------------------------
+
+
+#Set directory
+C:
+cd C:\Users\Windows\Desktop\server\
+
+#Warn you that the server will start.
+echo Starting in five seconds, press Ctrl-C to cancel.
+timeout 1
+echo Starting in four seconds, press Ctrl-C to cancel.
+timeout 1
+echo Starting in three seconds, press Ctrl-C to cancel.
+timeout 1
+echo Starting in two seconds, press Ctrl-C to cancel.
+timeout 1
+echo Starting in one second, press Ctrl-C to cancel.
+timeout 1 
+echo Starting, press Ctrl-C to cancel.
+
+
+#Starting the server
+java -Xms1G -Xmx2G -jar server.jar --nogui
+
+
 pause
