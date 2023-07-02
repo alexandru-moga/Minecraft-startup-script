5ce9ec903f701a7b9d8a6ca192191eb65c56b75e
The-tired-gamer <98146541+The-tired-gamer@users.noreply.github.com>
2023-07-02 14:11:23 +0300

    Update start.sh
    Added restart if crash and Aikar's Flags.

--- start.sh
+++ start.sh
@@ -1,15 +1,9 @@
-#Create screen
-screen -S minecraft_server
-screen -r minecraft_server
+#!/bin/bash
 
 #Check the java version
-echo "--------------------------------------------------------------------------------------------------"
+echo "-------------------------------------------------------------------------------"
 java --version
-echo "--------------------------------------------------------------------------------------------------"
-
-#Set directory
-cd /home/users/linux/minecraft_Server
-
+echo "-------------------------------------------------------------------------------"
 #Warn you that the server will start.
 echo "Starting in five seconds, press Ctrl-C to cancel."
 sleep 1
@@ -22,7 +16,12 @@ sleep 1
 echo "Starting in one second, press Ctrl-C to cancel."
 sleep 1
 echo "Starting, press Ctrl-C to cancel."
-
+echo "-------------------------------------------------------------------------------"
 
 #Starting the server
-java -Xms1G -Xmx2G -jar server.jar --nogui
+while [ true ]; do
+    java -Xms8192M -Xmx8192M --add-modules=jdk.incubator.vector -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -jar server.jar
+
+    echo Server restarting...
+    echo Press CTRL + C to stop.
+done
