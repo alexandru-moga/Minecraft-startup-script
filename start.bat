532ed4f2d90e1b41df6ec809376728b85dd069a7
The-tired-gamer <98146541+The-tired-gamer@users.noreply.github.com>
2023-07-02 14:14:15 +0300

    Update start.bat
    Added restart if a crash accured and Aikar's Flags.

--- start.bat
+++ start.bat
@@ -3,24 +3,22 @@
 #Set window title
 title Server Console
 
-
 #Check the java version
 echo --------------------------------------------------------------------------------------------------
 java --version
 echo --------------------------------------------------------------------------------------------------
 
-
-#Set directory
-C:
-cd C:\Users\Windows\Desktop\server\
-
 #Warn you that the server will start.
 timeout 5 
 echo Starting, press Ctrl-C to cancel.
-
+echo --------------------------------------------------------------------------------------------------
 
 #Starting the server
-java -Xms1G -Xmx2G -jar server.jar --nogui
+:start
+java -Xms8192M -Xmx8192M --add-modules=jdk.incubator.vector -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -jar server.jar --nogui
 
+echo Server restarting...
+echo Press CTRL + C to stop.
+goto :start
 
 pause
